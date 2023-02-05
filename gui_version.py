acb3c3a5a5765f915fcaf570e2b582208217cd74
the programmer <88006978+mitodocpp@users.noreply.github.com>
2023-02-05 13:11:37 -0300

    Update gui_version.py

--- gui_version.py
+++ gui_version.py
@@ -1,22 +1,25 @@
 from tkinter import *
 
-
 frame = Tk(className="calculator")
 frame.config(bg="gray")
-frame.geometry("500x80")
+frame.geometry("300x300")
 
-calc_input = Text(frame, height=1,width=50, bg="gray")
-calc_input.pack()
-def calc():
+result_label = Label(frame, text="", bg="gray", fg="white")
+operation = Text(frame, width=30, height=1, bg="gray", fg="white")
+
+def calculate():
     global result_label
-    result = calc_input.get(1.0, "end-1c")
-    result_label.config(text="result: " + str(eval(result)), bg="black", fg="white")
+    global operation
+    result_label.config(text="result: " + str(eval(operation.get(1.0, "end-1c"))))
     result_label.pack()
 
-calculate = Button(frame, text="calculate", command=calc, bg="black", fg="white")
-calculate.pack()
 
-result_label = Label(frame, text="", bg="gray")
-result_label.pack()
+orientation = Label(frame, text="enter the operation below:", bg="gray", fg="white")
+orientation.pack()
+
+operation.pack()
+
+calculate = Button(frame, text="calculate", bg="gray", fg="white", command=calculate)
+calculate.pack()
 
 frame.mainloop()
